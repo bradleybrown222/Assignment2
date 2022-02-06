124ce2dae2697c08e62beca21fef63bc0ec1ea38
Bradley Brown <brownleroy41@ymail.com>
2022-02-06 01:39:16 -0500

    added additions to rating view

--- HIITFit/Views/RatingView.swift
+++ HIITFit/Views/RatingView.swift
@@ -47,6 +47,6 @@ struct RatingView: View {
 struct RatingView_Previews: PreviewProvider {
   static var previews: some View {
     RatingView()
-      .previewLayout(.sizeThatFits)
+          .previewLayout(.sizeThatFits)
   }
 }
