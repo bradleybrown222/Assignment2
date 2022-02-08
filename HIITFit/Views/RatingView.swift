d66c991dbb300773cf2e6a27e52d691939768021
Bradley Brown <brownleroy41@ymail.com>
2022-02-07 23:31:31 -0500

    rating view changes

--- HIITFit/Views/RatingView.swift
+++ HIITFit/Views/RatingView.swift
@@ -32,11 +32,13 @@
 
 import SwiftUI
 
+
 struct RatingView: View {
   var body: some View {
     HStack {
+        
       ForEach(0 ..< 5) { _ in
-        Image(systemName: "waveform.path.ecg")
+          Image(systemName: "waveform.path.ecg")
           .foregroundColor(.gray)
           .font(.largeTitle)
       }
